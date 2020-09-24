1a22efc50e2720a759fb866682d708de3eaa6a1a
Michael Balcerzak <balcerzak.michael@gmail.com>
2020-09-24 13:39:42 -0400

    connect the project to the firebase

--- app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/MainActivity.java
+++ app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
