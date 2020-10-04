d5d7629bdc8c8d6dbc648fd6832acbd904c2e90e
Filipp G <8738425+filipp-g@users.noreply.github.com>
2020-10-04 15:04:25 -0400

    add basic task activity

---
+++ app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/TaskActivity.java
@@ -0,0 +1,14 @@
+package ca.carleton.comp3004f20.androidteamalpha.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
