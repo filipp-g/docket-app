64847449a06720ad84e60194331fd88a7aad4df8
Filipp G <8738425+filipp-g@users.noreply.github.com>
2020-10-11 15:42:31 -0400

    refactor project to extend task

--- app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/Project.java
+++ app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/Project.java
@@ -1,50 +1,28 @@
 package ca.carleton.comp3004f20.androidteamalpha.app;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
-public class Project {
+public class Project extends Task {
 
-    private long project_id;
-    private String project_name;
-    private List<Task> project_tasks;
-    private int time_required;
-    private int time_spent;
-    private boolean complete;
+    private List<Task> projectTasks;
 
-    public Project(long project_id, String project_name, List<Task> project_tasks,
-                   int time_required, int time_spent) {
-        this.project_id = project_id;
-        this.project_name = project_name;
-        this.project_tasks = project_tasks;
-        this.time_required = time_required;
-        this.time_spent = time_spent;
-        complete = false;
+    public Project() {
+        this.projectTasks = new ArrayList<>();
     }
 
-    public void addTask(Task task) {
-        this.project_tasks.add(task);
+    public Project(long id, String name, Project project, Date dueDate, float weight,
+                   int timeRequired, int timeSpent, List<Task> projectTasks) {
+        super(id, name, project, dueDate, weight, timeRequired, timeSpent);
+        this.projectTasks = projectTasks;
     }
 
-    public void removeTask(Task task) {
-        this.project_tasks.remove(task);
+    public List<Task> getProjectTasks() {
+        return projectTasks;
     }
 
-    public void removeTask(int counter) {
-        this.project_tasks.remove(counter);
+    public void setProjectTasks(List<Task> projectTasks) {
+        this.projectTasks = projectTasks;
     }
-
-    public Map<String, Object> return_task() {
-        HashMap<String, Object> task_To_Database = new HashMap<>();
-        task_To_Database.put("project id", project_id);
-        task_To_Database.put("project name", project_name);
-        task_To_Database.put("project tasks", project_tasks);
-        task_To_Database.put("time required", time_required);
-        task_To_Database.put("time spent", time_spent);
-        task_To_Database.put("complete", complete);
-
-        return task_To_Database;
-    }
-
 }
--- app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/Task.java
+++ app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/Task.java
@@ -1,51 +1,102 @@
 package ca.carleton.comp3004f20.androidteamalpha.app;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Task {
 
-    private long task_id;
-    private String task_name;
-    private Project task_project;
-    private Date due_date;
+    private long id;
+    private String name;
+    private Project project;
+    private Date dueDate;
     private float weight;
-    private int time_required;
-    private int time_spent;
+    private int timeRequired;
+    private int timeSpent;
     private boolean complete;
 
-    public Task(long task_id, String task_name, Project task_project, Date due_date, float weight,
-                int time_required, int time_spent) {
-        this.task_id = task_id;
-        this.task_name = task_name;
-        this.task_project = task_project;
-        this.due_date = due_date;
+    public Task() {
+        this.id = 0;
+        this.name = "";
+        this.project = null;
+        this.dueDate = new Date();
+        this.weight = 0;
+        this.timeRequired = 0;
+        this.timeSpent = 0;
+        this.complete = false;
+    }
+
+    public Task(long id, String name, Project project, Date dueDate, float weight,
+                int timeRequired, int timeSpent) {
+        this.id = id;
+        this.name = name;
+        this.project = project;
+        this.dueDate = dueDate;
         this.weight = weight;
-        this.time_required = time_required;
-        this.time_spent = time_spent;
-        complete = false;
+        this.timeRequired = timeRequired;
+        this.timeSpent = timeSpent;
+        this.complete = false;
     }
 
-    public Map<String, Object> return_task() {
-        HashMap<String, Object> task_To_Database = new HashMap<>();
-        task_To_Database.put("task id", task_id);
-        task_To_Database.put("task name", task_name);
-        task_To_Database.put("task project", task_project);
-        task_To_Database.put("due date", due_date);
-        task_To_Database.put("weight", weight);
-        task_To_Database.put("time required", time_required);
-        task_To_Database.put("time spent", time_spent);
-        task_To_Database.put("complete", complete);
-
-        return task_To_Database;
+    public long getId() {
+        return id;
     }
 
-    public long returnDueDate() {
-        return due_date.getTime();
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public String returnName() {
-        return task_name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getTimeRequired() {
+        return timeRequired;
+    }
+
+    public void setTimeRequired(int timeRequired) {
+        this.timeRequired = timeRequired;
+    }
+
+    public int getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(int timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
     }
 }
