679ddc998ed907d87eacc308e52e6cc48b289c07
Matteo Filia <matteo@matteofilia.com>
2020-11-14 06:01:00 -0500

    Implement AsyncCoundownTimerTask and create layout for time logging page

--- app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/TimerFragment.java
+++ app/src/main/java/ca/carleton/comp3004f20/androidteamalpha/app/TimerFragment.java
@@ -1,15 +1,29 @@
 package ca.carleton.comp3004f20.androidteamalpha.app;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.concurrent.ExecutionException;
+
+import static java.lang.Math.ceil;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,16 +31,70 @@ import com.google.firebase.auth.FirebaseAuth;
  * create an instance of this fragment.
  */
 public class TimerFragment extends Fragment {
-    private static final String EMAIL = "email";
-    private static final String USER= "user";
 
+    // FIREBASE VARIABLES
+    private static final String EMAIL = "email";
+    private static final String USER = "user";
     private String email;
     private String user;
 
+    // TIMER VARIABLES
+    private FloatingActionButton fab;
+    private TextView timerCounterText;
+    private EditText timerEditText;
+    private int timerMinutes = 30;
+    private boolean timerStopped = false;
+    private FrameLayout mainView;
+
+    // ASYNC VARIABLES
+    private AsyncCountdownTimerTask timerTask;
+
+    // TIMER FUNCTIONS
+    private void showTimerEditDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_edit_timer, null, false);
+        final EditText timerMinsEditText = (EditText) view.findViewById(R.id.timer_mins_edit_text);
+        timerMinsEditText.setText(String.valueOf(timerMinutes));
+        dialogBuilder.setView(view);
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+        dialog.setOnDismissListener(d -> {
+            timerMinutes = Integer.parseInt(timerMinsEditText.getText().toString());
+            resetTimer();
+        });
+    }
+    private void startTimer() {
+        fab.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_stop, getActivity().getTheme()));
+        timerStopped = false;
+        timerTask = new AsyncCountdownTimerTask();
+        timerTask.execute(timerMinutes);
+    }
+    private void stopTimer() {
+        if (timerTask != null) {
+            if (timerTask.isRunning()) timerTask.setRunning(false);
+        }
+
+        fab.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_play, getActivity().getTheme()));
+        timerStopped = true;
+    }
+    private void logTime() {
+        // Todo: log time to firebase / etc
+        Snackbar.make(mainView, "Logged " + String.valueOf(timerMinutes) + " minutes", Snackbar.LENGTH_SHORT).show();
+    }
+    private void updateTimer(int minutes, int seconds) {
+        timerCounterText.setText(StringUtils.leftPad(String.valueOf(minutes), 2, "0") + ":" + StringUtils.leftPad(String.valueOf(seconds), 2, "0"));
+    }
+    private void resetTimer() {
+        stopTimer();
+        updateTimer(timerMinutes, 0);
+    }
+
+    // FRAGMENT SETUP FUNCTIONS
     public TimerFragment() {
         // Required empty public constructor
     }
-
     public static TimerFragment newInstance(String email, String user) {
         TimerFragment fragment = new TimerFragment();
         Bundle args = new Bundle();
@@ -35,29 +103,121 @@ public class TimerFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
 
-        if (getArguments() != null) {
-            email = getArguments().getString(EMAIL);
-            user = getArguments().getString(USER);
+        // Get component ids
+        {
+            fab = view.findViewById(R.id.fab_play);
+            timerCounterText = view.findViewById(R.id.timer_counter_text);
+            timerEditText = view.findViewById(R.id.timer_edit_text);
+            mainView = view.findViewById(R.id.main);
         }
 
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, SignInFragment.newInstance(email, user)).commit();
-            Toast.makeText(getActivity(), "Please sign in...", Toast.LENGTH_SHORT).show();
-        } else {
-            // TODO
+        // Setup action listeners
+        {
+            fab.setOnLongClickListener(v -> {
+                resetTimer();
+                logTime();
+                return true;
+            });
+
+            fab.setOnClickListener(v -> {
+                if (timerStopped) startTimer();
+                else stopTimer();
+            });
+
+            timerEditText.setFocusableInTouchMode(false);
+            timerEditText.setOnClickListener(v -> showTimerEditDialog());
+            timerCounterText.setOnClickListener(v -> showTimerEditDialog());
+
+            if (getArguments() != null) {
+                email = getArguments().getString(EMAIL);
+                user = getArguments().getString(USER);
+            }
         }
+
+        // Setup Firebase
+        {
+            if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, SignInFragment.newInstance(email, user)).commit();
+                Toast.makeText(getActivity(), "Please sign in...", Toast.LENGTH_SHORT).show();
+            } else {
+                // TODO
+            }
+        }
+
         return view;
+    }
+
+    // Countdown Timer Task
+    private class AsyncCountdownTimerTask extends AsyncTask<Integer, Integer, Boolean> {
+
+        private static final int CHECKS_PER_SECOND = 30;
+        private boolean running = true;
+        private int totalSeconds;
+
+        public synchronized void setRunning(boolean running) {
+            this.running = running;
+        }
+
+        public boolean isRunning() {
+            return running;
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... integers) {
+            totalSeconds = integers[0]*60;
+            int seconds = totalSeconds;
+            int secondsLast = seconds;
+            long millisStart = System.currentTimeMillis();
+
+            while (seconds > 0) {
+                long sleepTimeMillis = 1000/CHECKS_PER_SECOND;
+
+                try {
+                    Thread.sleep(sleepTimeMillis);
+                } catch (InterruptedException e) {
+                    // Stop execution
+                    break;
+                }
+
+                // Todo: fix this code, timer will be off <1 second
+                long millisCurrent = System.currentTimeMillis();
+                if (running) {
+                    seconds = totalSeconds - (int)ceil((double)(millisCurrent - millisStart) / 1000);
+
+                    if (seconds < secondsLast) {
+                        secondsLast = seconds;
+                        publishProgress(seconds);
+                    }
+                    else continue;
+                } else {
+                    break;
+                }
+            }
+
+            return seconds == 0;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            updateTimer(values[0] / 60, values[0] % 60);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean notInterrupted) {
+            super.onPostExecute(notInterrupted);
+
+            if (notInterrupted) {
+                resetTimer();
+                logTime();
+            }
+        }
     }
 }
